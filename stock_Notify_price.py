e88a86e7e6f4685d73f1e7ec3574b9b14a910fed
Mz7414 <95566213+Mz7414@users.noreply.github.com>
2023-03-26 15:31:26 +0800

    Update stock_Notify_price.py

--- stock_Notify_price.py
+++ stock_Notify_price.py
@@ -17,66 +17,6 @@ import pandas as pd
 
 url=[f"https://histock.tw/stock/rank.aspx?&p={i}&d=1" for i in range(1,45)]
 
-def line(a,b,c) :
-    url = f'http://jsjustweb.jihsun.com.tw/z/zc/zcl/zcl.djhtm?a={a}&c={b}&d={c}'
-    resp = requests.get(url)
-
-    result = pd.read_html(resp.text)
-    df = result[2]        
-    df = df.drop([1, 2, 3,4,5,6,7,8,10],axis=1)         #刪除多餘直行
-    df = df.drop([0, 1, 2,3,4,5,6,df.shape[0]-1]) #刪除多餘橫排
-    
-    finNumber = df.shape[0]
-    df.columns=['日期','外資持股比例']  #重新命名columns名稱
-    df.reindex(df.index[::-1])     #反轉dataframe
-    df = df.reset_index(drop=True)     #重置資料編號(因為前面有刪掉多餘資料，故編號須重設) drop=True表示不保留原來編號
-
-    x2 = [float('{:.4f}'.format(float(df['外資持股比例'][i].strip('%'))/100)) for i in range(finNumber)] #取出dataframe中的外資比例
-
-    for i in range(finNumber) :
-        df['日期'][i] = df['日期'][i].replace(df['日期'][i][:3], str(int(df['日期'][i][:3])+1911)) #民國年轉為西元年(方便圖表顯示)
-    df['日期'] = pd.to_datetime(df['日期'])  #轉為日期格式(不轉的話圖表會很醜)
-
-    df.insert(1,column='外資持股比例以小數表示',value=x2)
-
-    fig, ax1 = plt.subplots()
-    plt.title('stock')
-    plt.xlabel('date')
-    ax2 = ax1.twinx()
-
-    ax1.set_ylabel('StockRate', color='tab:blue')
-    ax1.plot(df["日期"],df['外資持股比例以小數表示'], color='tab:blue')
-    ax1.tick_params(axis='y', labelcolor='tab:blue')
-    
-    ax2.set_ylabel('price', color='tab:orange')
-    ax2.plot(df["日期"],x, color='tab:orange')
-    ax2.tick_params(axis='y', labelcolor='tab:orange')
-    
-    ax = plt.gca()
-    tick_spacing = df.index.size/3 # x軸密集度
-    ax.xaxis.set_major_locator(mticker.MultipleLocator(tick_spacing))
-    
-    fig.tight_layout()
-    plt.grid()
-    plt.show()
-    plt.savefig('s.jpg')
-    image = open('s.jpg', 'rb')
-    url = 'https://notify-api.line.me/api/notify'
-    token = '5YapilxtO1ZnjlWTXgMfHq8N7wl4yXRbLRboMrFBVpE'
-    headers = {
-        'Authorization': 'Bearer ' + token    # 設定權杖
-    }
-    data = {
-        'message': 
-        "\n"+
-        f'{stockid}{name_dict[stockid]}'+'\n'+
-        f'股價已降至{y}元'+'\n'+
-        f'http://jsjustweb.jihsun.com.tw/z/zc/zcl/zcl.djhtm?a={stockid}&c={start}&d={end}'
-        
-    }
-    file = {image}
-    data = requests.post(url, headers=headers, data=data, files=file)
-
 def stock(i):
     global code,name
     resp=requests.get(url[i])
@@ -189,7 +129,21 @@ fin_list = [df_list[z[i]] for i in range(len(z))]
 
 
 #檢驗資料並傳送到line
-    
+def line() :
+    url = 'https://notify-api.line.me/api/notify'
+    token = '5YapilxtO1ZnjlWTXgMfHq8N7wl4yXRbLRboMrFBVpE'
+    headers = {
+        'Authorization': 'Bearer ' + token    # 設定權杖
+    }
+    data = {
+        'message': 
+        "\n"+
+        f'{stockid}{name_dict[stockid]}'+'\n'+
+        f'股價已降至{y}元'+'\n'+
+        f'http://jsjustweb.jihsun.com.tw/z/zc/zcl/zcl.djhtm?a={stockid}&c={start}&d={end}'        
+    }
+    data = requests.post(url, headers=headers, data=data)
+
 for i in range(len(fin_list)) :
     x=list(fin_list[i]["close"])
     y=x[-1]
@@ -206,6 +160,6 @@ for i in range(len(fin_list)) :
         df.reset_index(drop=True,inplace=True)
         x = [float('{:.2f}'.format(float(df[9][i].strip('%')))) for i in range(len(df))] #取出dataframe中的外資比例
         if max(x)-x[0]>=3 :
-            line(stockid,start,end)
+            line()
             time.sleep(0.7)
 
